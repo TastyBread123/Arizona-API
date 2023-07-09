36b35079c6dede79af058dc57148314d75369202
TastyBread123 <117034601+TastyBread123@users.noreply.github.com>
2023-07-09 20:53:20 +0300

    update example

--- examples/category_threads.py
+++ examples/category_threads.py
@@ -7,6 +7,6 @@ cookies = {"xf_user": "your",
 
 api = arz_api.ArizonaAPI(user_agent="your", cookie=cookies)
 
-for thread_id in api.get_category(1583).get_threads():
+for thread_id in api.get_threads(1583):
     thread = api.get_thread(thread_id)
     print(f"{thread.title} by {thread.creator.username}")
