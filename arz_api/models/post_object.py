cfe375f9b9e0e500fbd898979d3642a0cf8f83d3
TastyBread123 <117034601+TastyBread123@users.noreply.github.com>
2023-07-10 12:26:30 +0300

    v1.3

---
+++ arz_api/models/post_object.py
@@ -0,0 +1,75 @@
+from bs4 import BeautifulSoup
+from typing import TYPE_CHECKING
+
+from arz_api.consts import MAIN_URL
+if TYPE_CHECKING:
+    from arz_api import ArizonaAPI
+    from arz_api.models import Member, Thread
+
+
+class Post:
+    def __init__(self, API: 'ArizonaAPI', id: int, creator: 'Member', thread: 'Thread', create_date: int, bb_content: str) -> None:
+        self.API = API
+        self.id = id
+        self.creator = creator
+        self.thread = thread
+        self.create_date = create_date
+        self.bb_content = bb_content
+
+    def react(self, reaction_id: int) -> bool:
+        """Поставить реакцию на пост"""
+
+        token = BeautifulSoup(self.API.session.get(f"{MAIN_URL}/help/terms/").content, 'lxml').find('html')['data-csrf']
+        self.API.session.post(f'{MAIN_URL}/posts/{self.id}/react?reaction_id={reaction_id}', {'_xfToken': token})
+        return True
+    
+    def edit(self, html_message: str):
+        """Отредактировать пост"""
+
+        token = BeautifulSoup(self.API.session.get(f"{MAIN_URL}/help/terms/").content, 'lxml').find('html')['data-csrf']
+        self.API.session.post(f"{MAIN_URL}/posts/{self.id}/edit", {"message_html": html_message, "message": html_message, "_xfToken": token})
+
+    def delete(self, reason: str, hard_delete: int = 0) -> bool:
+        """Удалить пост"""
+
+        token = BeautifulSoup(self.API.session.get(f"{MAIN_URL}/help/terms/").content, 'lxml').find('html')['data-csrf']
+        self.API.session.post(f"{MAIN_URL}/posts/{self.id}/delete", {"reason": reason, "hard_delete": hard_delete, "_xfToken": token})
+        return True
+
+
+class ProfilePost:
+    def __init__(self, API: 'ArizonaAPI', id: int, creator: 'Member', profile: 'Member', create_date: int, bb_content: str, text_content: str) -> None:
+        self.API = API
+        self.id = id
+        self.creator = creator
+        self.profile = profile
+        self.create_date = create_date
+        self.bb_content = bb_content
+        self.text_content = text_content
+
+    def react(self, reaction_id: int) -> bool:
+        """Поставить реакцию на сообщение профиля"""
+
+        token = BeautifulSoup(self.API.session.get(f"{MAIN_URL}/help/terms/").content, 'lxml').find('html')['data-csrf']
+        self.API.session.post(f'{MAIN_URL}/profile-posts/{self.id}/react?reaction_id={reaction_id}', {'_xfToken': token})
+        return True
+
+    def comment(self, message_html: str) -> bool:
+        """Поставить комментарий на сообщение профиля"""
+
+        token = BeautifulSoup(self.API.session.get(f"{MAIN_URL}/help/terms/").content, 'lxml').find('html')['data-csrf']
+        self.API.session.post(f"{MAIN_URL}/profile-posts/{self.id}/add-comment", {"message_html": message_html, "_xfToken": token})
+        return True
+
+    def delete(self, reason: str, hard_delete: int = 0) -> bool:
+        """Удалить сообщение профиля"""
+
+        token = BeautifulSoup(self.API.session.get(f"{MAIN_URL}/help/terms/").content, 'lxml').find('html')['data-csrf']
+        self.API.session.post(f"{MAIN_URL}/profile-posts/{self.id}/delete", {"reason": reason, "hard_delete": hard_delete, "_xfToken": token})
+        return True
+    
+    def edit(self, html_message: str):
+        """Отредактировать сообщение профиля"""
+
+        token = BeautifulSoup(self.API.session.get(f"{MAIN_URL}/help/terms/").content, 'lxml').find('html')['data-csrf']
+        self.API.session.post(f"{MAIN_URL}/profile-posts/{self.id}/edit", {"message_html": html_message, "message": html_message, "_xfToken": token})
---
+++ arz_api/models/thread_object.py
@@ -0,0 +1,105 @@
+from typing import TYPE_CHECKING
+from bs4 import BeautifulSoup
+from re import compile
+
+from arz_api.consts import MAIN_URL
+if TYPE_CHECKING:
+    from arz_api.models.member_object import Member
+    from arz_api.models.category_object import Category
+    from arz_api import ArizonaAPI
+
+
+class Thread:
+    def __init__(self, API: 'ArizonaAPI', id: int, creator: 'Member', category: 'Category', create_date: int, title: str, content: str, html_content: str, pages_content: int, thread_post_id: int, is_closed: bool) -> None:
+        self.API = API
+        self.id = id
+        self.creator = creator
+        self.category = category
+        self.create_date = create_date
+        self.title = title
+        self.content = content
+        self.content_html = html_content
+        self.pages_count = pages_content
+        self.is_closed = is_closed
+        self.thread_post_id = thread_post_id
+
+    def answer(self, html_message: str) -> bool:
+        """Оставить ответ в теме"""
+
+        token = BeautifulSoup(self.API.session.get(f"{MAIN_URL}/help/terms/").content, 'lxml').find('html')['data-csrf']
+        self.API.session.post(f"{MAIN_URL}/threads/{self.id}/add-reply", {'_xfToken': token, 'message_html': html_message})
+        return True
+
+
+    def watch(self, stop: bool, email_subscribe: bool = False) -> bool:
+        """Отслеживание темы"""
+
+        token = BeautifulSoup(self.API.session.get(f"{MAIN_URL}/help/terms/").content, 'lxml').find('html')['data-csrf']
+        self.API.session.post(f"{MAIN_URL}/threads/{self.id}/watch", {'_xfToken': token, 'stop': int(stop), 'email_subscribe': int(email_subscribe)})
+        return True
+    
+
+    def close(self) -> bool:
+        """Закрыть тему (для модерации)"""
+
+        token = BeautifulSoup(self.API.session.get(f"{MAIN_URL}/help/terms/").content, 'lxml').find('html')['data-csrf']
+        self.API.session.post(f"{MAIN_URL}/threads/{self.id}/quick-close", {'_xfToken': token})
+        return True
+
+
+    def pin(self) -> bool:
+        """Закрепить тему (для модерации)"""
+
+        token = BeautifulSoup(self.API.session.get(f"{MAIN_URL}/help/terms/").content, 'lxml').find('html')['data-csrf']
+        self.API.session.post(f"{MAIN_URL}/threads/{self.id}/quick-stick", {'_xfToken': token})
+        return True
+    
+
+    def edit(self, html_message: str) -> bool:
+        """Отредактировать тему"""
+
+        token = BeautifulSoup(self.API.session.get(f"{MAIN_URL}/help/terms/").content, 'lxml').find('html')['data-csrf']
+        self.API.session.post(f"{MAIN_URL}/posts/{self.thread_post_id}/edit", {"message_html": html_message, "message": html_message, "_xfToken": token})
+        return True
+
+
+    def edit_info(self, title: str = None, prefix_id: int = None) -> bool:
+        """Изменить заголовок и префикс темы"""
+
+        token = BeautifulSoup(self.API.session.get(f"{MAIN_URL}/help/terms/").content, 'lxml').find('html')['data-csrf']
+        data = {"_xfToken": token}
+
+        if title is not None: data.update({'title': title})
+        if prefix_id is not None: data.update({'prefix_id[]', prefix_id})
+
+        self.API.session.post(f"{MAIN_URL}/threads/{self.id}/edit", data)
+        return True
+
+
+    def delete(self, reason: str, hard_delete: int = 0) -> bool:
+        """Удалить тему"""
+
+        token = BeautifulSoup(self.API.session.get(f"{MAIN_URL}/help/terms/").content, 'lxml').find('html')['data-csrf']
+        self.API.session.post(f"{MAIN_URL}/threads/{self.id}/delete", {"reason": reason, "hard_delete": hard_delete, "_xfToken": token})
+        return True
+
+
+    def react(self, reaction_id: int) -> bool:
+        """Поставить реакцию на тему"""
+
+        token = BeautifulSoup(self.API.session.get(f"{MAIN_URL}/help/terms/").content, 'lxml').find('html')['data-csrf']
+        self.API.session.post(f'{MAIN_URL}/posts/{self.thread_post_id}/react?reaction_id={reaction_id}', {'_xfToken': token})
+        return True
+    
+
+    def get_posts(self, page: int = 1) -> list:
+        """Получить все посты из темы"""
+
+        soup = BeautifulSoup(self.API.session.get(f"{MAIN_URL}/threads/{self.id}/page-{page}").content, 'lxml')
+
+        return_data = []
+        for i in soup.find_all('article', {'id': compile('js-post-*')}):
+            if i['id'].startswith('js-post-') == False: continue
+            return_data.append(i['id'].strip('js-post-'))
+
+        return return_data
